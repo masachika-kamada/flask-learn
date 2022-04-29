5633174a4337c92a05553bb58f2f86a4d8558ee2
masachika <masachika.kmd@gmail.com>
2022-04-29 18:12:35 +0900

    add hello and routing

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/japan")
+def japan():
+    return "<p>Hello, Japan!</p>"
+
+
+@app.route("/america")
+def america():
+    return "<p>Hello, America!</p>"
+
+
+@app.route("/world")
+def world():
+    return "<p>Hello, World!</p>"
