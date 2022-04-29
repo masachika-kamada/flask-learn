832b4b7f1ea8a836e30af3242960ddd8d24d00aa
masachika <masachika.kmd@gmail.com>
2022-04-29 18:22:51 +0900

    可変URL

--- hello.py
+++ hello.py
@@ -3,16 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/japan/tokyo")
-def japan():
-    return "<p>Hello, tokyo in Japan!</p>"
-
-
-@app.route("/japan/nara")
-def nara():
-    return "<p>Hello, nara in Japan!</p>"
-
-
-@app.route("/japan/saitama")
-def saitama():
-    return "<p>Hello, saitama in Japan!</p>"
+@app.route("/japan/<city>")
+def japan(city):
+    return f"Hello, {city} in Japan!"
