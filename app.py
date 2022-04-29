6824b0dbce2e1da9d335a22a09427fd752965a4d
masachika <masachika.kmd@gmail.com>
2022-04-30 01:58:02 +0900

    Bootstrapの適用

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_bootstrap import Bootstrap
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import pytz
@@ -11,6 +12,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 app.config["SECRET_KEY"] = os.urandom(24)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
