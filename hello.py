31109ddac698095e748304130dbd742fcc852059
masachika <masachika.kmd@gmail.com>
2022-04-29 18:32:25 +0900

    add html

--- hello.py
+++ hello.py
@@ -5,4 +5,19 @@ app = Flask(__name__)
 
 @app.route("/japan/<city>")
 def japan(city):
-    return f"Hello, {city} in Japan!"
+    return f"<h1>Hello, {city} in Japan!</h1>"
+
+
+html = """
+<h1>見出し</h1>
+<ul>
+    <li>箇条書き1</li>
+    <li>箇条書き2</li>
+    <li>箇条書き3</li>
+</ul>
+"""
+
+
+@app.route("/")
+def hello():
+    return html
